6177579baa8fbf01ab4a90a3bd8e2fc85290525e
Ross Briden <rcbridendev@gmail.com>
2023-12-17 10:26:23 -0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Python Napkin
+
+Notes on Python concepts.
+
+## Index
+1. Concurrency: AsyncIO and Greenlets
---
+++ concurrency/asyncio_counter.py
@@ -0,0 +1,25 @@
+import asyncio
+
+class _Counter:
+    _CLS_COUNTER = 0 
+    def __init__(self) -> None:
+        self.count = 0 
+
+    def inc(self):
+        self.count += 1 
+        _Counter._CLS_COUNTER += 1
+
+
+async def run_counter():
+    c = _Counter()
+
+    for _ in range(10):
+        await asyncio.sleep(0.1)
+        c.inc()
+    print(c.count)
+
+async def main():
+    await asyncio.gather(*(run_counter() for _ in range(10)))
+
+asyncio.run(main())
+print(_Counter._CLS_COUNTER)
